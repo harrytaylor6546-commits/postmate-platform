b86f76e823ac9dbe57a463e9a8d24252120a3a42
harrytaylor6546-commits <harry.taylor6546@gmail.com>
2026-06-05 19:02:42 +0200

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-
-export default async function Home() {
-  const { userId } = auth()
-  if (userId) redirect('/dashboard')
-  redirect('/sign-in')
-}
